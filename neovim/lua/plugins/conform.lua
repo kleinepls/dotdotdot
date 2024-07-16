4ae9491b77334248274e7edcc8c602d3e34fe1c6
Renatas <renatasgal420@gmail.com>
2024-07-16 12:43:51 +0300

    add formatter (conform.nvim) and linter (nvim-lint)

---
+++ neovim/lua/plugins/conform.lua
@@ -0,0 +1,25 @@
+return {
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { { "prettierd", "prettier" } },
+        typescript = { { "prettierd", "prettier" } },
+        javascriptreact = { { "prettierd", "prettier" } },
+        typescriptreact = { { "prettierd", "prettier" } },
+        vue = { { "prettierd", "prettier" } },
+      },
+    }
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*",
+      callback = function(args)
+        require("conform").format {
+          bufnr = args.buf,
+          lsp_format = "fallback",
+        }
+      end,
+    })
+  end,
+}
---
+++ neovim/lua/plugins/lint.lua
@@ -0,0 +1,24 @@
+return {
+  "mfussenegger/nvim-lint",
+  config = function()
+    require("lint").linters_by_ft = {
+      css = { "stylint" },
+      json = { "jsonlint" },
+      lua = { "stylua" },
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      vue = { "eslint_d" },
+    }
+
+    vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
+      callback = function()
+        local lint_status, lint = pcall(require, "lint")
+        if lint_status then
+          lint.try_lint()
+        end
+      end,
+    })
+  end,
+}
