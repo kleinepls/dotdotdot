d9053f35f4a645b4ae5dee6268740f14d5abc0d3
Renatas <renatasgal420@gmail.com>
2024-07-19 22:40:55 +0300

    update telescope keymaps

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -1,48 +1,67 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
   dependencies = {
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    -- 'nvim-telescope/telescope-ui-select.nvim',
-    'nvim-lua/plenary.nvim',
-    'echasnovski/mini.icons',
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-telescope/telescope-ui-select.nvim",
+    "nvim-lua/plenary.nvim",
+    "echasnovski/mini.icons",
   },
   config = function()
-    local telescope = require('telescope');
-    local builtin = require('telescope.builtin');
-    local utils = require('telescope.utils');
-    local themes = require('telescope.themes');
+    local telescope = require "telescope"
+    local builtin = require "telescope.builtin"
+    local utils = require "telescope.utils"
+    local themes = require "telescope.themes"
 
-    telescope.setup({
-      defaults = {
-        layout_config = {
-          prompt_position = 'top',
+    telescope.setup {
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown(),
         },
       },
-    });
+    }
 
-    telescope.load_extension('fzf');
+    telescope.load_extension "fzf"
+    telescope.load_extension "ui-select"
 
-    -- main keymaps
-    vim.keymap.set('n', '<C-n>', builtin.git_files, { desc = '[Telescope] Search Git files' })
-    vim.keymap.set('n', '<leader>pf', builtin.find_files);
-    vim.keymap.set('n', '<leader>pg', builtin.live_grep);
-    vim.keymap.set('n', '<leader>pc', builtin.commands)
-    vim.keymap.set('n', '<leader>pm', builtin.man_pages)
-    vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[Telescope] Find recently opened files' })
-    vim.keymap.set('n', '<leader>pr', builtin.resume, { desc = '[Telescope] Search Resume' })
-    vim.keymap.set('n', '<leader>pk', builtin.keymaps, { desc = '[Telescope] Search Keymaps' })
-    vim.keymap.set('n', '<leader>pq', builtin.quickfix, { desc = '[Telescope] Search Quickfix' })
+    vim.keymap.set("n", "<leader>fp", builtin.git_files)
+    vim.keymap.set("n", "<leader>ff", builtin.find_files)
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+    vim.keymap.set("n", "<leader>fc", builtin.commands)
+    vim.keymap.set("n", "<leader>fs", builtin.builtin)
+    vim.keymap.set("n", "<leader>fm", builtin.man_pages)
+    vim.keymap.set("n", "<leader>?", builtin.oldfiles)
+    vim.keymap.set("n", "<leader>fr", builtin.resume)
+    vim.keymap.set("n", "<leader>fk", builtin.keymaps)
+    vim.keymap.set("n", "<leader>fq", builtin.quickfix)
 
-    vim.keymap.set('n', '<leader>af', function() builtin.find_files({ cwd = utils.buffer_dir() }); end);
-    vim.keymap.set('n', '<leader>ag', function() builtin.live_grep({ cwd = utils.buffer_dir() }); end);
-    vim.keymap.set('n', '<leader>pw', function() builtin.grep_string({ search = vim.fn.expand '<cword>' }) end)
+    vim.keymap.set("n", "<leader>fw", function()
+      local word = vim.fn.expand "<cword>"
+      builtin.grep_string { search = word, prompt_title = "word: " .. word }
+    end)
 
-    vim.keymap.set('n', '<leader>/', function()
-      builtin.current_buffer_fuzzy_find(themes.get_dropdown { previewer = builtin.file_browser, })
-    end, { desc = '[/] Fuzzily search in current buffer' })
+    vim.keymap.set("n", "<leader>af", function()
+      builtin.find_files { cwd = utils.buffer_dir(), prompt_title = utils.buffer_dir() }
+    end)
+    vim.keymap.set("n", "<leader>ag", function()
+      builtin.live_grep { cwd = utils.buffer_dir(), prompt_title = utils.buffer_dir() }
+    end)
+
+    vim.keymap.set("n", "<leader>/", function()
+      builtin.current_buffer_fuzzy_find(themes.get_dropdown { previewer = builtin.file_browser })
+    end)
 
     -- misc
-    vim.keymap.set('n', '<leader>pn', function() builtin.find_files({ cwd = '~/dotfiles/neovim' }); end);
+    vim.keymap.set("n", "<leader>fn", function()
+      builtin.find_files { cwd = "~/dotfiles/neovim", prompt_title = "~/dotfiles/neovim" }
+    end)
+
+    -- git
+    vim.keymap.set("n", "<leader>gc", builtin.git_commits)
+    vim.keymap.set("n", "<leader>gh", builtin.git_bcommits)
+    vim.keymap.set("n", "<leader>gb", builtin.git_branches)
+    vim.keymap.set("n", "<leader>gt", builtin.git_status)
+    vim.keymap.set("n", "<leader>gx", builtin.git_stash)
   end,
 }
