5038846a91d436dcb6badd8b4f7223793fcec079
Renatas <renatasgal420@gmail.com>
2024-09-15 21:15:53 +0300

    add lsp_lines

--- neovim/lua/plugins/lsp_utils.lua
+++ neovim/lua/plugins/lsp_utils.lua
@@ -12,4 +12,25 @@ return {
       vim.keymap.set("n", "K", require("pretty_hover").hover)
     end,
   },
+
+  {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    opts = {},
+    init = function()
+      local virtual_text = true
+
+      vim.keymap.set("n", "<leader>dg", function()
+        require("lsp_lines").toggle()
+
+        virtual_text = not virtual_text
+        vim.diagnostic.config {
+          virtual_text = virtual_text,
+        }
+      end)
+
+      vim.diagnostic.config {
+        virtual_lines = false,
+      }
+    end,
+  },
 }
