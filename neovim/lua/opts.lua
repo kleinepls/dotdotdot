88084b60f5ab6b51bc94b2afcc63d0e5a245a600
Renatas <renatasgal420@gmail.com>
2024-10-08 16:46:00 +0300

    nvim enable nu & dont detach tmux after killing (a session)

--- neovim/lua/opts.lua
+++ neovim/lua/opts.lua
@@ -9,8 +9,7 @@ vim.opt.ignorecase = true
 vim.opt.incsearch = true
 vim.opt.isfname:append "@-@"
 vim.opt.mouse = "a"
--- vim.opt.nu = true
--- vim.opt.rnu = true
+vim.opt.nu = true
 vim.opt.scrolloff = 8
 vim.opt.shiftwidth = 4
 vim.opt.signcolumn = "auto"
