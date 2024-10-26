1bd7f695848a47333ade2a942b0bcdb55a1e7968
Renatas <renatasgal420@gmail.com>
2024-10-26 22:11:08 +0300

    add filetype-aware logging keymap for js and go

--- neovim/lua/keymaps.lua
+++ neovim/lua/keymaps.lua
@@ -2,7 +2,6 @@
 -- defaults, builtins
 --
 vim.keymap.set("n", "<Esc>", vim.cmd.nohlsearch)
-vim.keymap.set("n", "<Enter>", "<nop>")
 vim.keymap.set("n", "<space>", "<nop>")
 vim.keymap.set("v", "<space>", "<nop>")
 
@@ -54,9 +53,6 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>er", "oif err != nil {<Enter>}<Esc>Oreturn nil<Esc>")
 vim.keymap.set("n", "<leader>ej", '_yiw~$a `json:"<Esc>pa"`<Esc>V=')
 
--- javascript
-vim.keymap.set("n", "<leader>cl", "oconsole.log()<Esc>i")
-
 -- vue
 vim.keymap.set("n", "<leader>vs", 'i<script setup lang="ts"><CR><CR></script><CR><CR><template><CR></template><Esc>O')
 vim.keymap.set(
@@ -64,3 +60,29 @@ vim.keymap.set(
   "<leader>vc",
   'i<template><CR></template><CR><CR><script lang="ts"><CR></script><Esc>Osetup(props, { emit }) {<CR>}<Esc>O'
 )
+
+--
+--
+vim.keymap.set("n", "<leader>co", function()
+  local ft = vim.bo.filetype
+  local word = vim.fn.expand "<cword>"
+
+  if ft == "javascript" or ft == "typescript" then
+    if word == "" then
+      vim.cmd "normal oconsole.log()"
+      vim.cmd "startinsert"
+    else
+      vim.cmd("normal oconsole.log(" .. word .. ");")
+    end
+    return
+  end
+  if ft == "go" then
+    if word == "" then
+      vim.cmd "normal ofmt.Println()"
+      vim.cmd "startinsert"
+    else
+      vim.cmd("normal ofmt.Println(" .. word .. ")")
+    end
+    return
+  end
+end)
