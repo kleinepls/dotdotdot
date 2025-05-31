7a0bc6c4a2200ee3a35a38d6426cbd993291ad64
Renatas <renatasgal420@gmail.com>
2025-06-01 00:30:11 +0300

    turn off completion menu auto show

--- neovim/lua/plugins/cmp.lua
+++ neovim/lua/plugins/cmp.lua
@@ -12,13 +12,13 @@ return {
     },
     completion = {
       documentation = { auto_show = true },
-      ghost_text = { enabled = true },
+      -- ghost_text = { enabled = true },
       accept = { auto_brackets = { enabled = false } },
-
       menu = {
-        auto_show = function(ctx)
-          return ctx.mode ~= "cmdline"
-        end,
+        auto_show = false,
+        -- auto_show = function(ctx)
+        --   return ctx.mode ~= "cmdline"
+        -- end,
 
         draw = {
           columns = { { "label", "label_description", gap = 1 }, { "kind_icon", gap = 1, "kind" } },
