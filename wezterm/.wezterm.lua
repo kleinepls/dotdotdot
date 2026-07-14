8d8989fdc0d396139d8908247ceeda48dfab9642
Renatas <renatasgal420@gmail.com>
2026-07-14 11:41:46 +0300

    fix opt arrow movement in m*cos

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -16,6 +16,9 @@ return {
 	harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }, -- turn off the disgusting ligatures
 
 	keys = {
-		{ key = "Backspace", mods = "CTRL", action = wezterm.action.SendKey({ key = "w", mods = "CTRL" }) },
+		{ key = "Backspace",  mods = "CTRL", action = wezterm.action.SendKey({ key = "w", mods = "CTRL" }) },
+
+		{ key = 'LeftArrow',  mods = 'OPT',  action = wezterm.action.SendString '\x1b[1;5D' },
+		{ key = 'RightArrow', mods = 'OPT',  action = wezterm.action.SendString '\x1b[1;5C' },
 	},
 }
